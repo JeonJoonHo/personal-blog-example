1dcc420439382eae032f313a18fe07beef6ed83e
Jeon Joonho <jeon.joonho@nbt.com>
2021-08-24 17:57:30 +0900

    change generation type for database pk

--- src/main/java/com/example/blog/post/Post.java
+++ src/main/java/com/example/blog/post/Post.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 @Builder @AllArgsConstructor @NoArgsConstructor
 public class Post {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
--- src/main/java/com/example/blog/user/User.java
+++ src/main/java/com/example/blog/user/User.java
@@ -14,7 +14,7 @@ import java.util.List;
 @AllArgsConstructor @NoArgsConstructor
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String type;
